d48d94aca68875134247eaa7c62779d1fbadad37
Hamidur Rahman <Hamidur0405@gmail.com>
2019-10-12 23:43:59 -0400

    adding demo files and test functions

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 
 from Util import return_lowered_lines
 from Util import return_all_files
+from Util import make_lines_from_dir
+from Util import make_words
+import os
 
 # 12,500 files
 TRAIN_POS = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/train/pos/"
@@ -9,14 +12,22 @@ TRAIN_NEG = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb
 # 1 file, #89527 lines
 VOCAB = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/imdb.vocab"
 
-vocabularies = return_lowered_lines(VOCAB)
-print(len(vocabularies))
+# vocabularies = return_lowered_lines(VOCAB)
+#
+# pos_train_files = return_all_files(TRAIN_POS)
+#
+# neg_train_files = return_all_files(TRAIN_NEG)
 
-pos_train_files = return_all_files(TRAIN_POS)
-print(len(pos_train_files))
 
-neg_train_files = return_all_files(TRAIN_NEG)
-print(len(neg_train_files))
+pos_train_all_lines = make_lines_from_dir(TRAIN_POS)
+print(len(pos_train_all_lines))
+words = make_words(pos_train_all_lines)
+print(len(words))
+
+neg_train_all_lines = make_lines_from_dir(TRAIN_NEG)
+print(len(neg_train_all_lines))
+words = make_words(neg_train_all_lines)
+print(len(words))
 
 
 
--- Util.py
+++ Util.py
@@ -17,7 +17,12 @@ def return_all_files(file_dir):
 
 
 def make_words(lines):
-    pass
+    words = list()
+    for line in lines:
+        splits = line.split()
+        filtered = list(filter(None, splits))
+        words.extend(filtered)
+    return words
 
 
 def remove_punctuations(words):
@@ -29,5 +34,14 @@ def remove_punctuations(words):
     return new_words
 
 
+def make_lines_from_dir(file_dir):
+    files = return_all_files(file_dir)
+    all_lines = list()
+    for file in files:
+        file_path = file_dir+file
+        f = open(file_path)
+        all_lines.extend(return_lowered_lines(file_path))
+        f.close()
+    return all_lines
 
 
