127408570dc8e59d2ccf35dd0e2d1bb60657962f
Hamidur Rahman <Hamidur0405@gmail.com>
2019-10-25 23:32:54 -0400

    removing some functions

--- Extra.py
+++ Extra.py
@@ -1,9 +1,7 @@
 
 
 from Util import *
-import datetime
 import difflib
-from Classes import PreProcess
 
 
 def cal_prob_sent(processed_classifier_obj, test_sent, total_files, final_merged_map):
@@ -20,115 +18,6 @@ def cal_prob_sent(processed_classifier_obj, test_sent, total_files, final_merged
 def test():
     t = "The]. rain\\in //Spain` a----f. 'a'a' miller-mac.<br *k &i'm= a''''b [a,h p]"
     print(punctuations_regex(t.split()))
-
-
-def do_small_corpus():
-    action_train = os.getcwd()+"/small-corpus/train/action/"
-    comedy_train = os.getcwd()+"/small-corpus/train/comedy/"
-
-    action_comedy = os.getcwd()+"/output-demo/action/comedy.txt"
-    comedy_action = os.getcwd()+"/output-demo/comedy/action.txt"
-
-    # clear_file(action_comedy)
-    # clear_file(comedy_action)
-
-    processed_train_action = PreProcess(action_train)
-    processed_train_comedy = PreProcess(comedy_train)
-    merged = merge1(processed_train_action.words_map, processed_train_comedy.words_map)
-    total_files = processed_train_action.total_files+processed_train_comedy.total_files
-    print(cal_prob_sent(processed_train_action, "fast couple shoot fly", total_files, merged))
-    print(cal_prob_sent(processed_train_comedy, "fast couple shoot fly", total_files, merged))
-
-
-def do_original(PreProcess):
-    print("PROGRAM STARTED")
-    train_pos = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/train/pos/"
-    train_neg = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/train/neg/"
-
-    test_pos = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/test/pos/"
-    test_neg = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/test/neg/"
-
-    pos_neg = os.getcwd()+"/output/pos/neg.txt"
-    neg_pos = os.getcwd()+"/output/neg/pos.txt"
-
-    a = datetime.datetime.now()
-
-    clear_file(pos_neg)
-    clear_file(neg_pos)
-
-    processed_train_pos = PreProcess(train_pos)
-    processed_train_neg = PreProcess(train_neg)
-    merged = merge1(processed_train_pos.words_map, processed_train_neg.words_map)
-    total_words = len(merged)
-    write_to_file_map(merged)
-
-    print("time taken to train the model", datetime.datetime.now()-a)
-
-    total_files = processed_train_pos.total_files + processed_train_neg.total_files
-
-    class_predictor_dir(processed_train_pos, processed_train_neg, test_pos, total_files, total_words,
-                        pos_neg)
-    class_predictor_dir(processed_train_neg, processed_train_pos, test_neg, total_files, total_words,
-                        neg_pos)
-
-    pos_in_neg = len(return_lowered_lines(neg_pos))
-    neg_in_pos = len(return_lowered_lines(pos_neg))
-    print("Out of all test-pos -> ", processed_train_pos.total_files, " : ", neg_in_pos, " are neg")
-    print("Out of all test-neg -> ", processed_train_neg.total_files, " : ", pos_in_neg, " are pos")
-
-    print("given all test-pos files, my model predicted that it is : ",
-          accuracy(neg_in_pos, processed_train_pos.total_files), "% accurate")
-
-    print("given all test-neg files, my model predicted that it is : ",
-          accuracy(pos_in_neg, processed_train_neg.total_files), "% accurate")
-
-    print(datetime.datetime.now()-a)
-    print("PROGRAM FINISHED")
-
-
-def do_original_keep_pun(PreProcess):
-    print("PROGRAM STARTED")
-    train_pos = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/train/pos/"
-    train_neg = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/train/neg/"
-
-    test_pos = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/test/pos/"
-    test_neg = "/Users/hamidurrahman/Downloads/CSCI381/hw2/movie-review-HW2/aclImdb/test/neg/"
-
-    pos_neg = os.getcwd()+"/output/pos/neg.txt"
-    neg_pos = os.getcwd()+"/output/neg/pos.txt"
-
-    a = datetime.datetime.now()
-
-    clear_file(pos_neg)
-    clear_file(neg_pos)
-
-    processed_train_pos = PreProcess(train_pos)
-    processed_train_neg = PreProcess(train_neg)
-    merged = merge1(processed_train_pos.words_map, processed_train_neg.words_map)
-    total_words = len(merged)
-
-    print("time taken to train the model:", datetime.datetime.now()-a)
-
-    total_files = processed_train_pos.total_files + processed_train_neg.total_files
-
-    class_predictor_dir(processed_train_pos, processed_train_neg, test_pos, total_files, total_words,
-                        pos_neg)
-    class_predictor_dir(processed_train_neg, processed_train_pos, test_neg, total_files, total_words,
-                        neg_pos)
-
-    pos_in_neg = len(return_lowered_lines(neg_pos))
-    neg_in_pos = len(return_lowered_lines(pos_neg))
-    print("Out of all test-pos -> ", processed_train_pos.total_files, " : ", neg_in_pos, " are neg")
-    print("Out of all test-neg -> ", processed_train_neg.total_files, " : ", pos_in_neg, " are pos")
-
-    print("given all test-pos files, my model predicted that it is : ",
-          accuracy(neg_in_pos, processed_train_pos.total_files), "% accurate")
-
-    print("given all test-neg files, my model predicted that it is : ",
-          accuracy(pos_in_neg, processed_train_neg.total_files), "% accurate")
-
-    print(datetime.datetime.now()-a)
-    print("PROGRAM FINISHED")
 
 
 def merge(map1, map2):
@@ -260,5 +149,3 @@ def class_predictor_file_by_num(class1_obj, class2_obj, test_file, total_train_f
     else:
         return 0
 
-
-do_small_corpus()
